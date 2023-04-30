c09f0b260ff9ce35db386a95feba816469b8f275
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-30 16:15:05 -0300

    learning python syntax/1

--- chapter-01/helloWorld.py
+++ chapter-01/helloWorld.py
@@ -1,5 +1,10 @@
+# este é um comentário e o código abaixo cria uma variável e a exibe na tela
 nome = 'João Felipe Zini'
 print('Olá,', nome, 'seja bem-vindo!')
 
 pessoa = 'João'
 print('Olá,', pessoa)
+
+numero = 1
+numero += 2
+print(numero)
