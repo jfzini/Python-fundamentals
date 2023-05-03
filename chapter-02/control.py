e29bf87f7e94dc86846a7886d851a64e1b7ef07e
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-03 11:38:02 -0300

    range and list

--- chapter-02/control.py
+++ chapter-02/control.py
@@ -8,7 +8,9 @@ while counter <= n:
     print(sum)
 print(sum)
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+arr = range(1, 16)  # this is a range object and its iterable
+arr = list(range(1, 16))  # this is a list made with the range obj = array
 sum_arr = 0
 
 for c in arr:  # only iterable objects
