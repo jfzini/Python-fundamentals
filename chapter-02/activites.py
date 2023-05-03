409c087b57eb5463922c63599055ca7be4c6fafa
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-03 12:10:37 -0300

    exercises

--- chapter-02/activites.py
+++ chapter-02/activites.py
@@ -23,3 +23,41 @@ divisor = 3
 for numero in range(0, 1000):
     if numero % 3 == 0:
         print(numero)
+
+# variáveis do tipo string
+nome = 'João da Silva'
+cidade = 'São Paulo'
+cpf = '123.456.789-00'
+
+# Transforme todos os caracteres das variáveis em maiúsculo;
+string_list = [nome, cidade, cpf]
+upper_case_list = []
+
+for string in string_list:
+    upper_case_list.append(string.upper())
+
+print(upper_case_list)
+
+# Transforme todos os caracteres das variáveis em minúsculo;
+lower_case_list = []
+
+for string in string_list:
+    lower_case_list.append(string.lower())
+
+print(lower_case_list)
+
+# Exiba a posição do caractere ã, se presente, em cada uma das variáveis;
+
+for string in string_list:
+    print(string.find('ã'))
+
+# Exiba o número de caracteres de cada variável;
+
+for string in string_list:
+    print(len(string))
+
+# Remova os pontos (.) e o hífen (–) da variável cpf.
+replacements = {'-': '', '.': ''}
+for old, new in replacements.items():
+    cpf = cpf.replace(old, new)
+print(cpf)
