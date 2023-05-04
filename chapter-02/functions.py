ddc7519742dcb1af083c5ca281791418d41f4480
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-03 22:20:59 -0300

    exercising functions in py

--- chapter-02/functions.py
+++ chapter-02/functions.py
@@ -3,3 +3,12 @@ def sum(a, b):
 
 
 print(sum(4, 5))
+
+
+def highest_number(list):
+    max_number = max(list)
+    times = list.count(max_number)
+    return f"The highest number in the given list is {max_number} and it shows up {times} times"
+
+
+print(highest_number([1, 2, 3, 4, 4, 4, 3, 2, 3, 3, 3]))
