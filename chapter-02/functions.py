c34cacadb41d7df21ac661326560c4538dbef33c
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-03 23:04:43 -0300

    discovering the number of possible combinations for the lottery using itertools

--- chapter-02/functions.py
+++ chapter-02/functions.py
@@ -1,12 +1,13 @@
-from control import arr
-import activities as act
+# from control import arr
+# import activities as act
+import itertools
 
 
 def sum(a, b):
     return a + b
 
 
-print(sum(4, 5))
+# print(sum(4, 5))
 
 
 def highest_number(list):
@@ -15,6 +16,11 @@ def highest_number(list):
     return f"The highest number in the given list is {max_number} and it shows up {times} times"
 
 
-print(highest_number([1, 2, 3, 4, 4, 4, 3, 2, 3, 3, 3]))
-print(highest_number(arr))
-print(highest_number(range(act.inicio, act.fim)))
+# print(highest_number([1, 2, 3, 4, 4, 4, 3, 2, 3, 3, 3]))
+# print(highest_number(arr))
+# print(highest_number(range(act.inicio, act.fim)))
+numbers_list = [0, 1, 3, 5, 7, 9]
+megasena = list(range(1, 61))
+print(len(list(itertools.combinations(numbers_list, 4))))
+print(len(list(itertools.permutations(numbers_list, 6))))
+print(len(list(itertools.combinations(megasena, 6))))
